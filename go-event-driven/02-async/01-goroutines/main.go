ca0516fdde154a036e5eb020578b892c5e5d1c02
Juan Gonzalo Quıroz Cadavıd <jquirozcadavid@gmail.com>
2025-06-05 13:55:51 +0300

    Go event driven

--- go-event-driven/02-async/01-goroutines/main.go
+++ go-event-driven/02-async/01-goroutines/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"log"
+	"sync"
+	"time"
+)
+
 type User struct {
 	Email string
 }
@@ -39,13 +45,47 @@ func (h Handler) SignUp(u User) error {
 		return err
 	}
 
-	if err := h.newsletterClient.AddToNewsletter(u); err != nil {
-		return err
+	wg := sync.WaitGroup{}
+	functions := []func(u User) error{
+		h.newsletterClient.AddToNewsletter,
+		h.notificationsClient.SendNotification,
 	}
 
-	if err := h.notificationsClient.SendNotification(u); err != nil {
-		return err
+	// if err := h.newsletterClient.AddToNewsletter(u); err != nil {
+	// 	return err
+	// }
+
+	// if err := h.notificationsClient.SendNotification(u); err != nil {
+	// 	return err
+	// }
+
+	for _, f := range functions {
+		wg.Add(1)
+		go func(f func(u User) error) {
+			log.Println("Holi")
+			defer wg.Done()
+			h.runFunction(u, f)
+
+		}(f)
 	}
+
+	log.Println("Waiting")
+	wg.Wait()
+	log.Println("Done")
 
 	return nil
+}
+
+func (h Handler) runFunction(u User, f func(u User) error) {
+	timeToSleep := 100
+	for {
+		if err := f(u); err != nil {
+			log.Println("Sleeping for ", timeToSleep, " seconds")
+			time.Sleep(time.Duration(timeToSleep) * time.Millisecond)
+			continue
+		}
+
+		return
+	}
+
 }
